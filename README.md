18b1abe9b92ca046c2913f1d77b36bfb85c5716c
maxonemillion <max.daniel@utah.edu>
2020-08-29 20:58:05 -0600

    updated css, html, and java

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# weather-dashboard
+# WEATHER DASHBOARD
+
+## PURPOSE
+The purpose of this assigment was to create a functional weather application.
+
+## WHAT I LEARNED
+I learned a great deal about server-side APIs and JQuery syntax while doing this assignment. Each assignment proves more valuable than the last.
+
+## RUN
+To run this project, enter the name of the city in which you would like to know the weather.
+
+### SCREENSHOT
+![project screenshot](develop/images/screesnhot.png)
+
+## LINKS
+Deployable link:
+
+Github: 
--- develop/script.js
+++ develop/script.js
@@ -13,6 +13,13 @@ $('#day3').text(moment().add(3, 'days'));
 $('#day4').text(moment().add(4, 'days'));
 $('#day5').text(moment().add(5, 'days'));
 
+$('#locationsearch').keypress(function (event) {
+  if (event.keyCode === 13) {
+    $('#gobutton').click();
+  }
+});
+
+
 $(document).ready(function () {
 
   $('#gobutton').click(function () {
@@ -37,7 +44,11 @@ $(document).ready(function () {
       console.log(response);
 
       var temperature = response.main.temp 
-      $('#temperature').text(temperature);
+      $('#temperature').text((((((temperature - 273.15)*1.8) + 32)) + " ° F"));
+      var humidity = response.main.humidity
+      $('#humidity').text(humidity + " %");
+      var wind = response.wind.speed
+      $('#wind').text(wind + " mph");
       
       var lon = (response.coord.lon);
       var lat = (response.coord.lat);
@@ -49,6 +60,10 @@ $(document).ready(function () {
       }).then(function (response) {
         console.log(queryURL3);
         console.log(response);
+
+        var UV = response.value
+        $('#UV').text(UV);
+        
       });
     });
 
@@ -60,6 +75,44 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(queryURL2);
       console.log(response);
+
+      // HERE, FINISH
+
+      var temperature1 = response.zero.main.temp;
+      $('#temperature1').text(temperature1);
+      var humidity1 = response.zero.humidity;
+      $('#humidity1').text(humidity1);
+      var wind1 = response.zero.wind;
+      $('#wind1').text(wind1);
+
+      var temperature2 = response.six.main.temp;
+      $('#temperature2').text(temperature2);
+      var humidity2 = response.six.main.humidity;
+      $('#humidity2').text(humidity2);
+      var wind2 = response.six.wind;
+      $('#wind2').text(wind2);
+
+      var temperature3 = response.twelve.main.temp;
+      $('#temperature3').text(temperature3);
+      var humidity3 = response.twelve.main.humidity;
+      $('#humidity3').text(humidity3);
+      var wind3 = response.twelve.wind;
+      $('#wind3').text(wind3);
+
+      var temperature4 = response.eighteen.temp;
+      $('#temperature4').text(temperature4);
+      var humidity4 = response.eighteen.main.humidity;
+      $('#humidity4').text(humidity4);
+      var wind4 = response.eighteen.wind;
+      $('#wind4').text(wind4);
+
+      var temperature5 = response.twentyfour.main.temp;
+      $('#temperature5').text(temperature5);
+      var humidity5 = response.twentyfour.main.humidity;
+      $('#humidity5').text(humidity5);
+      var wind5 = response.twentyfour.wind;
+      $('#wind5').text(wind5);
+
     });
 
   }
